d06ad2e4a4c2e2b02c71d621cba589a89e1e4b1d
GianbaBotassi <gianba89@gmail.com>
2024-02-27 06:50:53 +0100

    add delete and clear

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -12,7 +12,7 @@ public class Calculator implements ActionListener{
     JButton decButton, equButton,delButton,clrButton;
     JPanel panel;
 
-    Font myFont = new Font("Ink Free", Font.BOLD,30);
+    Font myFont = new Font("Calibri", Font.BOLD,30);
 
     double num1=0,num2=0,result=0;
     char operator;
@@ -22,6 +22,52 @@ public class Calculator implements ActionListener{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,550);
         frame.setLayout(null);
+
+        textField = new JTextField();
+        textField.setBounds(50,25,300,50);
+        textField.setFont(myFont);
+        textField.setEditable(false);
+
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(",");
+        equButton = new JButton("=");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
+
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+
+        for (JButton butt:functionButtons
+             ) {
+            butt.addActionListener(this);
+            butt.setFont(myFont);
+            butt.setFocusable(false);
+        }
+
+        for(int i = 0;i<10;i++){
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false);
+        }
+
+        delButton.setBounds(50,430,145,50);
+        clrButton.setBounds(205,430,145,50);
+
+
+        frame.add(delButton);
+        frame.add(clrButton);
+        frame.add(addButton);
+        frame.add(textField);
         frame.setVisible(true);
     }
 
