ffd64f6e0cd22883e7dbca5b3b4d81d95055aa78
GianbaBotassi <gianba89@gmail.com>
2024-02-27 07:14:37 +0100

    Gui done

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,6 +4,7 @@ import java.awt.event.*;
 
 public class Calculator implements ActionListener{
 
+//  Initializing frame components
     JFrame frame;
     JTextField textField;
     JButton[] numberButtons = new JButton[10];
@@ -12,22 +13,27 @@ public class Calculator implements ActionListener{
     JButton decButton, equButton,delButton,clrButton;
     JPanel panel;
 
+//    Calculator font
     Font myFont = new Font("Calibri", Font.BOLD,30);
 
     double num1=0,num2=0,result=0;
     char operator;
 
+//    Constructor
     Calculator(){
+//        Setting window frame
         frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,550);
         frame.setLayout(null);
 
+//        Setting textfield
         textField = new JTextField();
         textField.setBounds(50,25,300,50);
         textField.setFont(myFont);
         textField.setEditable(false);
 
+//        Button list right side and bottom side
         addButton = new JButton("+");
         subButton = new JButton("-");
         mulButton = new JButton("*");
@@ -37,6 +43,7 @@ public class Calculator implements ActionListener{
         delButton = new JButton("Delete");
         clrButton = new JButton("Clear");
 
+//        Put into array functional buttons
         functionButtons[0] = addButton;
         functionButtons[1] = subButton;
         functionButtons[2] = mulButton;
@@ -46,6 +53,7 @@ public class Calculator implements ActionListener{
         functionButtons[6] = delButton;
         functionButtons[7] = clrButton;
 
+//      add listener for each functional button
         for (JButton butt:functionButtons
              ) {
             butt.addActionListener(this);
@@ -53,6 +61,7 @@ public class Calculator implements ActionListener{
             butt.setFocusable(false);
         }
 
+//      add listener for each number button
         for(int i = 0;i<10;i++){
             numberButtons[i] = new JButton(String.valueOf(i));
             numberButtons[i].addActionListener(this);
@@ -60,13 +69,35 @@ public class Calculator implements ActionListener{
             numberButtons[i].setFocusable(false);
         }
 
+//      Setting size and position
         delButton.setBounds(50,430,145,50);
         clrButton.setBounds(205,430,145,50);
 
+        panel = new JPanel();
+        panel.setBounds(50,100,300,300);
+        panel.setLayout(new GridLayout(4,4,10,10));
 
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numberButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
+
+
+        frame.add(panel);
         frame.add(delButton);
         frame.add(clrButton);
-        frame.add(addButton);
         frame.add(textField);
         frame.setVisible(true);
     }
